a55c07459c473ef85c715cd5a93f014229f1dbff
ksws330a <38136944+ksws330a@users.noreply.github.com>
2020-07-21 20:55:46 +0800

    Hystrix

--- springcloud-service-consumer/src/main/java/com/springcloud/consumer/controller/UserContorller.java
+++ springcloud-service-consumer/src/main/java/com/springcloud/consumer/controller/UserContorller.java
@@ -2,7 +2,6 @@ package com.springcloud.consumer.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.springcloud.consumer.client.UserClient;
 import com.springcloud.consumer.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
@@ -22,33 +21,18 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("consumer/user")
-//@DefaultProperties(defaultFallback = "fallBackMethod")	// 指定默认的降级方法
+@DefaultProperties(defaultFallback = "fallBackMethod")	// 指定默认的降级方法
 public class UserContorller {
-//	@Autowired
-//	private RestTemplate restTemplate;
+	@Autowired
+	private RestTemplate restTemplate;
 
 	@Autowired
 	private DiscoveryClient discoveryClient; // Eureka客户端，可以获取到服务实例信息
 
-	@Autowired
-	private UserClient userClient;
-
 	@GetMapping
 	@HystrixCommand
 	public User queryUserById(@RequestParam("id") String id) {
-//		if("1".equals(id)) {
-//			throw new RuntimeException();
-//		}
-		// 根据服务名称，获取服务实例
-//		List<ServiceInstance> instances = discoveryClient.getInstances("SERVICE-PROVIDE");
-		// 因为只有一个服务,直接get(0)获取
-//		ServiceInstance instance = instances.get(0);
-		// 获取ip和端口信息
-//		String baseUrl = "http://"+instance.getHost() + ":" + instance.getPort()+"/user/";
-//		return this.restTemplate.getForObject(baseUrl + id, User.class);
-//		return this.restTemplate.getForObject("http://localhost:8081/user/" + id, User.class);
-//		return this.restTemplate.getForObject("http://service-provide/user/" + id, User.class);
-		return userClient.queryUserById(id);
+		return this.restTemplate.getForObject("http://service-provide/user/" + id, User.class);
 	}
 
 	public User queryUserByIdFallBack(String id) {
